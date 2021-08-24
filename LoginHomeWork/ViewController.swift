1dc30b57fe09790dc374388f4380d1ab5b65102e
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-08-24 10:23:35 +0400

    Initial Commit

---
+++ LoginHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginHomeWork
+//
+//  Created by Nadzeya Shpakouskaya on 24/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
